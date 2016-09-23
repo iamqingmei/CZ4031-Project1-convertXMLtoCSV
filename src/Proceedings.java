c5b24072829e91ae67cd079d02e0668c514770af
LiChuqiao <Chuqiao@172.22.140.160>
2016-09-23 14:08:26 +0800

    Chuqiao

---
+++ src/Proceedings.java
@@ -0,0 +1,39 @@
+package main;
+
+public class Proceedings extends Publication {
+	private String isbn;
+	private String booktitle;
+	private String pulisher;
+
+	public Proceeedings(){
+		super();
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getBooktitle() {
+		return booktitle;
+	}
+
+	public void setBooktitle(String booktitle) {
+		this.booktitle = booktitle;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public String toString(){
+		return this.getPubId()+this.getPubKey()+this.getTitle()+this.getYear()+this.getIsbn()+this.getBooktitle()+this.getPublisher();
+	}
+}
